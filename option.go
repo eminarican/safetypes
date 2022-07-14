ed77dfcaafad1f23ebcc0a4dc85eb7d877d676f9
eminarican <emin.arican@outlook.com>
2022-07-14 13:53:09 +0300

    json & bson tags for option

--- option.go
+++ option.go
@@ -1,42 +1,42 @@
 package safetypes
 
 type Option[T any] struct {
-	value *T
+	Value *T `json:"value,omitempty" bson:"value,omitempty"`
 }
 
 func Some[T any](value T) Option[T] {
 	return Option[T]{
-		value: &value,
+		Value: &value,
 	}
 }
 
 func None[T any]() Option[T] {
 	return Option[T]{
-		value: nil,
+		Value: nil,
 	}
 }
 
 func (o Option[T]) Some(value T) Option[T] {
-	o.value = &value
+	o.Value = &value
 	return o
 }
 
 func (o Option[T]) None() Option[T] {
-	o.value = nil
+	o.Value = nil
 	return o
 }
 
 func (o *Option[T]) IsSome() bool {
-	return o.value != nil
+	return o.Value != nil
 }
 
 func (o *Option[T]) IsNone() bool {
-	return o.value == nil
+	return o.Value == nil
 }
 
 func (o *Option[T]) Unwrap() T {
 	if o.IsNone() {
 		panic("can't unwrap none value")
 	}
-	return *o.value
+	return *o.Value
 }
